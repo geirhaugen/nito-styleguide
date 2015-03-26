aa9293e149c1124e39931ec0e0ed451421d49c86
Geir Haugen <geir.haugen@epinova.no>
2015-03-26 09:11:38 +0100

    Last commit

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
         watch: {
           styles: {
             files: ['less/**/*.less', 'ejs/**/*.ejs'], // which files to watch
-            tasks: ['less', 'ejs:all', 'notify:less'],
+            tasks: ['less', 'ejs:all', 'copy:img', 'copy:gfx', 'copy:js', 'copy:fonts'],
             options: {
               nospawn: true
             }
@@ -26,16 +26,28 @@ module.exports = function(grunt) {
         },
         copy:{
             img: {
-                src: "img/*",
-                dest: "build/"
+                cwd: 'img/',  // set working folder / root to copy
+                src: '**/*',           // copy all files and subfolders
+                dest: 'build/img',    // destination folder
+                expand: true
             },
             gfx: {
-                src: "gfx/*",
-                dest: "build/"
+                cwd: 'gfx/',  // set working folder / root to copy
+                src: '**/*',           // copy all files and subfolders
+                dest: 'build/gfx',    // destination folder
+                expand: true
             },
             js:{
-                src: "js/*",
-                dest: "build/"
+                cwd: 'js/',  // set working folder / root to copy
+                src: '**/*',           // copy all files and subfolders
+                dest: 'build/js',    // destination folder
+                expand: true
+            },
+            fonts:{
+                cwd: 'fonts/',  // set working folder / root to copy
+                src: '**/*',           // copy all files and subfolders
+                dest: 'build/css/fonts/',    // destination folder
+                expand: true
             }
         },
         notify: {
@@ -46,25 +58,50 @@ module.exports = function(grunt) {
                 }
             }
         },
+        browserSync: {
+            bsFiles: {
+                src : 'build/ejs/*.html'
+            },
+            options: {
+                watchTask: true,
+                ghostmode: false,
+                server: {
+                    baseDir: "build/"
+                }
+            }
+        },
         ejs: {
             all: {
-                src: ['ejs/*.ejs'],
-                //cwd: 'ejs/*.html',
-                dest: 'build/',
+                src: ['*.ejs'],
+                cwd: 'ejs/',
+                dest: 'build',
                 expand: true,
                 ext: '.html',
                 options:{
-                    "title" : "testtittel",
-                    "modules" : [
-                        '_content/header',
-                        "_content/typography",
-                        "_content/grid",
-                        "_content/buttons",
-                        "_content/colors",
-                        "_content/article",
-                        "_content/course",
-                        "_content/frontpageNews",
-                        "_content/mainFooter"
+                    "title" : 'testtittel',
+                    "frontpage" : [
+                        '_content/mainheader',
+                        '_content/banner.ejs',
+                        '_content/article',
+                        '_content/frontpageNews',
+                        '_content/mainFooter'
+                    ],
+                    "course" :[
+                        '_content/mainheader',
+                        '_content/course',
+                        '_content/mainFooter'
+                    ],
+                    "styleguidemodules" : [
+                        '_content/mainheader',
+                        '_content/banner.ejs',
+                        '_content/typography',
+                        '_content/grid',
+                        '_content/buttons',
+                        '_content/colors',
+                        '_content/article',
+                        '_content/course',
+                        '_content/frontpageNews',
+                        '_content/mainFooter'
                     ]
                 }
             }
@@ -74,8 +111,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-ejs-locals');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
 
     
-    grunt.registerTask('default', ['less', 'ejs:all', 'copy:img', 'copy:gfx', 'copy:js', 'notify:less', 'watch']);
+    grunt.registerTask('default', [  'browserSync', 'notify:less', 'watch']);
 };
--- js/app.js
+++ js/app.js
@@ -12,7 +12,20 @@ $().ready(function () {
         $(this).parent().parent().toggleClass("viewHelpers");
     });
 
-    $(".js-menuToggler").click(function () {
-        $(".js-mainNavigationList").slideToggle();
+    $(".js-btn-menuToggler").click(function () {
+        $(".js-mainNavigationList").slideToggle().attr("aria-hidden","false");
+
     });
+
+    // Add a markup block after each live example container
+    $('.live-example').after("<div class='markup'></div>");
+
+// Fill the markup block with the code from the live container
+    $('.live-example').each(function () {
+        $(this).next($('.markup')).text($(this).html());
+    });
+
+    // Wrap the code in the markup block with a <pre> tag for prettyprinting
+    $('.markup').wrapInner('<pre class="prettyprint " />');
 });
+
